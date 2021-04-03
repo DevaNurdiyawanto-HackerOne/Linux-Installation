841594256a0be045b575b2fe063c12ee6892d4a8
DevaNurdiyawanto-HackerOne <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 01:35:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+apt-get install git php curl wget nano python python2 nmap ruby vim nodejs electrum build-essential cmake libjpeg-turbo libpng python make yarn perl sudo python-pip python2-pip python3-pip -y
