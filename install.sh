c0940148aebfc3838bd6d3f267857dfcf8097ecf
devanurdiyawanto <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-05 01:49:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt-get install git php curl wget nano python python2 nmap ruby vim nodejs electrum build-essential cmake libjpeg-turbo libpng python make yarn perl sudo python-pip python2-pip python3-pip nmap sqlmap nikto -y
+apt-get install git php curl wget nano python python2 nmap ruby vim nodejs build-essential cmake python make perl sudo python-pip python3-pip nmap sqlmap nikto -y
